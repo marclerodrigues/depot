c7ee91d3dfc32e8521c10d710a207e2d8ad6c863
Marcle Rodrigues <maarclee@gmail.com>
2016-06-03 11:31:37 -0300

    Create Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,6 +12,10 @@
 #
 
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :presence => true
   validates :title, :uniqueness => true
   validates :price, :numericality => { :greater_than_of_equal_to => 0.01 }
@@ -20,4 +24,13 @@ class Product < ActiveRecord::Base
     :message => 'Must be a URL for JPG ou PNG image.',
     :multiline => true
   }
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      erros.add(:base, 'Line items present')
+      return false
+    end
+  end
 end
